f5f95c48f10e310f22e14fea39c7e5bdc8bed7a0
sayemhaque <mdsayemofficial1000@gmail.com>
2023-05-14 14:00:32 +0600

    services api created

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const cors = require('cors');
 require("dotenv").config()
 const app = express();
@@ -26,7 +26,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    
+    const servicesCollection = client.db("car-doctor").collection("services")
+
+     app.get('/services' , async (req,res) => {
+        const cursor = await servicesCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+     } )
+
+    //  single service
+    
+    app.get('/services/:id' , async (req,res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const options = {
+            projection: {  title: 1, price: 1 , service_id:1},
+          };
+      
+        const result = await servicesCollection.findOne(query,options)
+        res.send(result) 
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
